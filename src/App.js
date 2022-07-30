56c4c123b42657dd9bb132b73ee14b966615218a
nacho1777 <60706560+nacho1777@users.noreply.github.com>
2022-07-29 23:03:09 -0300

    Nacho-test
    TEst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './clock';
+import React, { Component } from 'react';
+import Forecast from "./components/forecast/Forecast";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src= {logo} className= "App-logo" alt= "logo" />
+          <header><h2>Welcome to Nacho's App</h2></header>
+          <main>  
+            <Forecast />
+          </main>
+          <footer><h5>Brought to you by Nacho</h5></footer>
+        </div>
+          <p className= "App-intro">
+            Hi {this.props.name}!
+          </p>
+          <Clock ></Clock>
+      </div>     
+    );
+  }
 }
 
 export default App;
